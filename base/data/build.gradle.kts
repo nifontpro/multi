d6339d3feceb00fba033966e3d390d68ac409a79
Nifont <nifontbus@yandex.ru>
2024-02-19 19:53:06 +0300

    +spring cloud

--- base/data/build.gradle.kts
+++ base/data/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
 
+extra["springCloudVersion"] = Spring.CLOUD_VERSION
 dependencyManagement {
 	imports {
 		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${Spring.CLOUD_VERSION}")
